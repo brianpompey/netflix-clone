839fef1008d311e4420b4353ba6491044fd5a865
brianpompey <brianthescholar@gmail.com>
2021-09-08 22:54:44 -0400

    working with async await syntax

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+main().catch(err => console.log(err));
+
 async function main() {
     await mongoose.connect('process.env.MONGO_URL');
+
+    console.log("DB Connection Successful"));
   }
 
 
