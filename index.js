9d9b65849d996a1df5aa711cb6109f2865c6a75d
brianpompey <brianthescholar@gmail.com>
2021-09-08 22:49:59 -0400

    added env

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 
 async function main() {
-    await mongoose.connect('mongodb://localhost:27017/test');
+    await mongoose.connect('process.env.MONGO_URL');
   }
 
 
