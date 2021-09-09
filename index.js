edcabdb7173c6a7a1918811b6503d786e1b98ca3
brianpompey <brianthescholar@gmail.com>
2021-09-08 22:50:48 -0400

    vonfig for dotenv

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const app = express();
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 async function main() {
     await mongoose.connect('process.env.MONGO_URL');
